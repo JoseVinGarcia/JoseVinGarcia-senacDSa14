7b868415795ac576c39dd6d6385410af67fa902a
JoseVinGarcia <josevingarcia2020@gmail.com>
2024-10-29 19:57:30 -0300

    Conteúdo inicial da Aula 14

---
+++ Aula14/exemplo1_gitignore_venv.py
@@ -0,0 +1,7 @@
+print("Olá")
+
+# cd (nome da pasta)/ - Entra na pasta
+# cd .. - Sobe uma pasta
+# git add (nome da pasta)/(nome do arquivo) - Adiciona um arquivo dentro de uma pasta no stage
+# GITIGNORE:
+# *.docx - ignora QUALQUER arquivo com final Docx
