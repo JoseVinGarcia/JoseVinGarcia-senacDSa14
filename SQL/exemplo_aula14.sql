d006ef857aa6f082f0a237bcec68583b06699d77
JoseVinGarcia <josevingarcia2020@gmail.com>
2024-10-29 21:10:56 -0300

    Conteudo inicial sobre SQL

---
+++ SQL/exemplo_aula14.sql
@@ -0,0 +1,10 @@
+/* SELECT * FROM vendas */
+
+/* SELECT Produto, Categoria, DataVenda
+FROM vendas
+WHERE DataVenda > "2024-05-01" /* and Categoria = "Fotografia" */
+
+SELECT Produto, Valor
+FROM Vendas
+WHERE Valor > 100 AND Valor < 1200;
+
